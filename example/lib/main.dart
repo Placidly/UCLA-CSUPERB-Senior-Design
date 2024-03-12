433277d279795fae57f56810c3ccff1f1771ecce
Placidly <jhuang117@calstatela.edu>
2024-03-12 15:52:06 -0700

    added colorScheme in MaterialApp

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 // import 'package:telematics_sdk_example/screens/title_screen.dart';
 import 'package:telematics_sdk_example/screens/welcome_screen.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -17,6 +17,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TelematicsSDK Example',
+      theme: ThemeData(
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Color(0xff627CB2),
+          onPrimary: Colors.black,
+          secondary: Color(0xffD3A94A),
+          onSecondary: Colors.black,
+          tertiary: Color(0xff1A2B4E),
+          error: Colors.red,
+          onError: Colors.white,
+          background: Colors.white,
+          onBackground: Colors.black,
+          surface: Colors.white,
+          onSurface: Colors.black,
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: WelcomeScreen(),
     );
   }
